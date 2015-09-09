64c0e771f41bfcd9108963c3b16a7958e727c858
Victor Hugo <xblader@gmail.com>
2015-09-09 20:28:18 -0300

    end of article 1 - connection

---
+++ ConnectMongo/ConnectMongo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace ConnectMongo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             IMongoClient _client;
+             IMongoDatabase _database;
+
+            _client = new MongoClient();
+            _database = _client.GetDatabase("test");
+        }
+    }
+}
